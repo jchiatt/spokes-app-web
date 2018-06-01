c885995aeb4bce50667bd023e82d04cbdd975db9
J.C. Hiatt <jchiatt@me.com>
2018-06-01 17:04:57 -0500

    stub out content for forecast screen

---
+++ client/src/components/forecast/Day.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Day = () => {
+  return(
+    <div>
+      <h3>Saturday, June 2</h3>
+      <p>High: 72 &#8457; / 22 &#8451;</p>
+      <p>Low: 68 &#8457; / 20 &#8451;</p>
+      <p>Wind speed: 4-8 mph</p>
+      <p>Precipitation: highly unlikely.</p>
+
+      <button>Mark as good</button>
+    </div>
+  )
+}
+
+export default Day;
--- client/src/screens/Forecast.js
+++ client/src/screens/Forecast.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
+import Day from '../components/forecast/Day';
+
 const Forecast = () => {
-  return <h1>Forecast</h1>
+  return (
+    <div>
+      <h1>Forecast</h1>
+
+      <h2>Over the next 7 days, here's the best days to ride</h2>
+
+      <Day />
+      <Day />
+
+      <h2>These days don't look so good</h2>
+
+      <Day />
+      <Day />
+      <Day />
+
+    </div>
+  )
 }
 
 export default Forecast;
