9e8c2aabb29095c22a3b98b1e9b73ebf2bc2f4d3
J.C. Hiatt <jchiatt@me.com>
2018-06-02 13:56:22 -0500

    create test api call to world weather online API and add basic input validation for zip code

--- client/src/components/today/LocationSearch.js
+++ client/src/components/today/LocationSearch.js
@@ -1,31 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class LocationSearch extends Component {
-  constructor() {
-    super();
+  state = {
+    zip: ''
+  }
+
+  updateZip = ( event ) => {
+    !isNaN(event.target.value) ? this.setState( { zip: event.target.value }) : console.log(event.target.value = " is not a number");
+  }
+
+  handleZipSubmit = ( event ) => {
+    event.preventDefault();
     
-    this.state = {
-      zip: ''
-    }
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange( event ) {
-    this.setState( { zip: event.target.value });
-  }
-
-  handleSubmit( event ) {
-    alert('A ZIP was submitted: ' + this.state.zip);
+    const API_BASE  = "https://api.worldweatheronline.com/premium/v1/weather.ashx";
+    const API_KEY   = "dac7582d3a0c433db2c25733180206";
+    
+    axios.get("https://api.worldweatheronline.com/premium/v1/weather.ashx?key=" + API_KEY + "&q=" + this.state.zip + "&num_of_days=1&format=json")
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err))
+    
   }
   
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleZipSubmit}>
           <label>Zip code:
-            <input type="text" value={this.state.zip} onChange={this.handleChange} />
+            <input type="text" pattern="[0-9]{5}" value={this.state.zip} onChange={this.updateZip} />
           </label>
 
           <input type="submit" value="Submit" />
