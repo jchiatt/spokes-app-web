3e2516e72947c0e0efb6db6ae90d58a673e65a1b
J.C. Hiatt <jchiatt@me.com>
2018-06-03 20:40:16 -0500

    create appsync client and create apollo provider

--- client/src/index.js
+++ client/src/index.js
@@ -7,5 +7,28 @@ import './styles/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import AWSAppSyncClient from 'aws-appsync';
+import { Rehydrated } from 'aws-appsync-react';
+import { ApolloProvider } from 'react-apollo';
+
+import appSyncConfig from './api/AppSync';
+
+const client = new AWSAppSyncClient({
+  url: appSyncConfig.graphqlEndpoint,
+  region: appSyncConfig.region,
+  auth: {
+    type: appSyncConfig.authenticationType,
+    apiKey: appSyncConfig.apiKey,
+  },
+});
+
+const WithProvider = () => (
+  <ApolloProvider client={client}>
+    <Rehydrated>
+      <App />
+    </Rehydrated>
+  </ApolloProvider>
+);
+
+ReactDOM.render(<WithProvider />, document.getElementById('root'));
 registerServiceWorker();
