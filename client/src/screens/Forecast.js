81649241545ccc2d65ab9456aa77b32e5432406f
J.C. Hiatt <jchiatt@me.com>
2018-06-05 13:41:19 -0500

    escape HTML entities in Forecast component

--- client/src/screens/Forecast.js
+++ client/src/screens/Forecast.js
@@ -6,12 +6,12 @@ const Forecast = () => (
   <div>
     <h1>Forecast</h1>
 
-    <h2>Over the next 7 days, here's the best days to ride</h2>
+    <h2>Over the next 7 days, here&apos;s the best days to ride</h2>
 
     <Day />
     <Day />
 
-    <h2>These days don't look so good</h2>
+    <h2>These days don&apos;t look so good</h2>
 
     <Day />
     <Day />
