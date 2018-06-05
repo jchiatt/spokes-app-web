52062eda940e06729c0bfeef56d13b62d39a568a
J.C. Hiatt <jchiatt@me.com>
2018-06-05 13:38:50 -0500

    prettify Rank component for eslint

--- client/src/components/today/Rank.js
+++ client/src/components/today/Rank.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
-const Rank = () => {
-  return(
-    <div>
-      <h3>How was your ride?</h3>
-      <button>Great</button>
-      <button>Good</button>
-      <button>Bad</button>
-      <button>Terrible</button>
-    </div>
-  )
-}
+const Rank = () => (
+  <div>
+    <h3>How was your ride?</h3>
+    <button>Great</button>
+    <button>Good</button>
+    <button>Bad</button>
+    <button>Terrible</button>
+  </div>
+);
 
 export default Rank;
